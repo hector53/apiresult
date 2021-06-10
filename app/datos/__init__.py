23cd4c917cd4e15672c5c265e6e74c3d447d02cb
hector53 <hectoracosta5@gmail.com>
2021-06-10 07:28:02 -0400

    2do

--- app/datos/__init__.py
+++ app/datos/__init__.py
@@ -1,18 +1,18 @@
 #datos produccion
-"""
+
 datos = {
    "host":"localhost",
     "database":"minipoll",
     "userDb" : "nacho",
     "userPass":"nacho*123",
 }
-url_site = "minipoll.jemyxs2xli-zqy3jz1yy6kg.p.runcloud.link"
-"""
+url_site = "apiresult.jemyxs2xli-zqy3jz1yy6kg.p.runcloud.link"
+
 
 
 
 #datos localhost
-
+"""
 datos = {
     "host":"localhost",
     "database":"minipoll",
@@ -21,7 +21,7 @@ datos = {
 }
 url_site = "http://127.0.0.1:5000/"
 
-
+"""
 
 #datos Host para db
 host=datos["host"]
