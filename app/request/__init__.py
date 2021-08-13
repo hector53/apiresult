0e30490ea89c67c19bf04c91f75bbaf1e58c73b0
hector53 <hectoracosta5@gmail.com>
2021-08-13 11:53:16 -0400

    iahsid

--- app/request/__init__.py
+++ app/request/__init__.py
@@ -1935,20 +1935,7 @@ def get_data_by_stripe():
     userData = getDataOne(sql)
     email = userData[3]
     customer = userData[12]
-    if customer != 0: 
-        session = stripe.checkout.Session.create(
-        success_url=url_site_front+'upgrade/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=url_site_front+'upgrade/error',
-        customer=customer,
-        payment_method_types=['card'],
-        mode='subscription',
-        line_items=[{
-        'price': price_id,
-        # For metered billing, do not pass quantity
-        'quantity': 1
-        }],
-        )
-    else:
+    if customer == 0: 
         session = stripe.checkout.Session.create(
         success_url=url_site_front+'upgrade/success?session_id={CHECKOUT_SESSION_ID}',
         cancel_url=url_site_front+'upgrade/error',
@@ -1961,6 +1948,20 @@ def get_data_by_stripe():
         'quantity': 1
         }],
         )
+    else:
+        session = stripe.checkout.Session.create(
+        success_url=url_site_front+'upgrade/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_site_front+'upgrade/error',
+        customer=customer,
+        payment_method_types=['card'],
+        mode='subscription',
+        line_items=[{
+        'price': price_id,
+        # For metered billing, do not pass quantity
+        'quantity': 1
+        }],
+        )
+        
         
     #guardar en la db le sesion generada al usuario para luego de pagado poder verificar si pago o no y aprobarle su suscripcion
     sql = f"""
