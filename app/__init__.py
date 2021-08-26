03662172833c77532e25f62208b153791f9309d8
hector53 <hectoracosta5@gmail.com>
2021-08-26 11:31:47 -0400

    hiihasd

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,11 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 
-app.config["JWT_SECRET_KEY"] = "8as4d8as4asd48asd8asd4asd8"  # Change this!
+app.config["JWT_SECRET_KEY"] = "8as4d8as4asd48$$***asd8asd4asd8"  # Change this!
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
 jwt = JWTManager(app)
 
-socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=10, ping_interval=5)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 from app.request import *
